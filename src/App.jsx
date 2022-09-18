bfdbe5475355d42dec5226fc5d23faf750bde279
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-09-17 20:35:11 -0500

    Terminamos el componente de contacto

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { About } from './components/About'
 import { Home } from './components/Home'
 import { Quotes } from './components/Quotes'
 import { Projects } from './components/Projects'
+import { Contact } from './components/Contact'
 
 export const App = () => {
 
@@ -28,6 +29,13 @@ export const App = () => {
           </Grid>
           <Grid item sx={{}}>
             <Quotes />
+          </Grid>
+          <Grid item sx={{}}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 310"><path fill="#1b1b1b" fillOpacity="1"   d="M0,224L60,208C120,192,240,160,360,165.3C480,171,600,213,720,202.7C840,192,960,128,1080,122.7C1200,117,  1320,171,1380,197.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,  480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+            </svg>
+          </Grid>
+          <Grid>
+            <Contact />
           </Grid>
         </Grid>
       </Box>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,42 @@
+import { Grid, IconButton, Typography } from "@mui/material"
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import GoogleIcon from '@mui/icons-material/Google';
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 export const Contact = () => {
+
   return (
-    <div>Contact</div>
+    <Grid container id='Contact' sx={{backgroundColor:'#1b1b1b', height:'120px', margin:'-55px 0'}} justifyContent='center' alignItems='center'>
+      <Grid item>
+        <IconButton size="large">
+          <a href='https:github.com/EduardoMsf' target='_blank'>
+            <GitHubIcon color='whiteSmoke' fontSize="large"/>
+          </a>
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton size="large">
+          <a href='https://www.linkedin.com/in/eduardomsf/' target='_blank'>
+            <LinkedInIcon color='whiteSmoke' fontSize="large" />
+          </a>
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton size="large">
+          <a href='https://www.instagram.com/samaniegomsf/' target='_blank'>
+            <InstagramIcon color='whiteSmoke' fontSize="large" />
+          </a>
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton size="large">
+          <GoogleIcon color='whiteSmoke' fontSize="large"/>
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <Typography sx={{color:'#f5f5f5'}}>samaniegomsf@gmail.com</Typography>
+      </Grid>
+    </Grid>
   )
 }
